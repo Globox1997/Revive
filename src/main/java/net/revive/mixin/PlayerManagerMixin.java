f017eb6ccf83ce63b1f4ee913f177b5c27caf24a
Globox1997 <mitschke11@web.de>
2021-10-01 12:36:15 +0200

    flush

--- src/main/java/net/revive/mixin/PlayerManagerMixin.java
+++ src/main/java/net/revive/mixin/PlayerManagerMixin.java
@@ -4,7 +4,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
@@ -14,10 +13,6 @@ import net.revive.packet.ReviveServerPacket;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
-
-    @Inject(method = "respawnPlayer", at = @At("HEAD"))
-    public void respawnPlayerMixin(ServerPlayerEntity player, boolean alive, CallbackInfoReturnable<ServerPlayerEntity> info) {
-    }
 
     @Inject(method = "onPlayerConnect", at = @At("TAIL"))
     private void onPlayerConnectMixin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
